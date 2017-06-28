bba79e914c3fba2cd2e9ba155b8652bd8eebebaf
qkstnrshfmswk <cjcj558@naver.com>
2017-06-28 11:06:58 +0900

    slide

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController,IonicPage} from 'ionic-angular';
 import {Service} from '../../app/service';
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
@@ -13,9 +14,24 @@ export class HomePage {
     featured: any[] = [];
     cartItems: any[];
     noOfItems: number;
+    
+    main_img;
+    
+    constructor(public navCtrl: NavController, public service: Service, public http:Http) {
+        this.http.get('http://ec2-34-224-40-186.compute-1.amazonaws.com:3000/main')
+        .subscribe(
+        data =>
+        {
+          this.main_img = data.json();
+          console.log(this.main_img);
+       
+        },
+        error =>
+        {
+          console.log("error");
+        });
 
 
-    constructor(public navCtrl: NavController, public service: Service) {
         this.cartItems = JSON.parse(localStorage.getItem('cartItem'));
         if (this.cartItems != null) {
             this.noOfItems = this.cartItems.length;
